6a732e8f4ba330e390033ab18b55fa379028c2eb
dzjnt <kosqer@wp.pl>
2020-04-26 09:49:02 +0200

    Fixed app

---
+++ WindowsFormsApp1/MessagesForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace MarnikProjekt
+{
+    partial class MainAppForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picturesListView = new System.Windows.Forms.ListView();
+            this.speakButton = new System.Windows.Forms.Button();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.clearListView = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // picturesListView
+            // 
+            this.picturesListView.HideSelection = false;
+            this.picturesListView.Location = new System.Drawing.Point(12, 12);
+            this.picturesListView.Name = "picturesListView";
+            this.picturesListView.Size = new System.Drawing.Size(1101, 359);
+            this.picturesListView.TabIndex = 0;
+            this.picturesListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // speakButton
+            // 
+            this.speakButton.Location = new System.Drawing.Point(158, 377);
+            this.speakButton.Name = "speakButton";
+            this.speakButton.Size = new System.Drawing.Size(133, 23);
+            this.speakButton.TabIndex = 1;
+            this.speakButton.Text = "Przeczytaj tekst";
+            this.speakButton.UseVisualStyleBackColor = true;
+            this.speakButton.Click += new System.EventHandler(this.speakButton_Click);
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(12, 377);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(140, 23);
+            this.loadButton.TabIndex = 3;
+            this.loadButton.Text = "Załaduj obrazek z dysku";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // clearListView
+            // 
+            this.clearListView.Location = new System.Drawing.Point(297, 377);
+            this.clearListView.Name = "clearListView";
+            this.clearListView.Size = new System.Drawing.Size(133, 23);
+            this.clearListView.TabIndex = 6;
+            this.clearListView.Text = "Wyczyść listę";
+            this.clearListView.UseVisualStyleBackColor = true;
+            this.clearListView.Click += new System.EventHandler(this.clearListView_Click);
+            // 
+            // MainAppForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1126, 414);
+            this.Controls.Add(this.clearListView);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.speakButton);
+            this.Controls.Add(this.picturesListView);
+            this.Name = "MainAppForm";
+            this.Text = "Aplikacja do rozpoznawania mowy";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView picturesListView;
+        private System.Windows.Forms.Button speakButton;
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.Button clearListView;
+    }
+}
+
---
+++ WindowsFormsApp1/MessagesForm.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MarnikProjekt
+{
+    public partial class MainAppForm : Form
+    {
+        public SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer();
+        public PromptBuilder PromptBuilder = new PromptBuilder();
+        public SpeechRecognitionEngine SpeechRecognitionEngine = new SpeechRecognitionEngine();
+        public ImageList images = new ImageList();
+        private OpenFileDialog OpenFileDialog;
+
+        public MainAppForm()
+        {
+            InitializeComponent();
+       
+        }
+        public void GetImagesFromOpenDialog(string fullPath)
+        {
+            if(picturesListView.Columns.Count == 0)
+            {
+                picturesListView.Columns.Add("Obrazki do przeczytania", 150);
+                picturesListView.AutoResizeColumn(0, ColumnHeaderAutoResizeStyle.HeaderSize);
+                picturesListView.View = View.Details;
+            }
+          
+            images.ImageSize = new Size(100, 100);
+
+            try
+            {              
+                var name = Path.GetFileName(fullPath);
+                images.Images.Add(name, Image.FromFile(fullPath));             
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+            picturesListView.SmallImageList = images;
+            AddImagesToListView(images);
+
+        }
+        public void AddImagesToListView(ImageList images)
+        {
+            //Usunięcie rozszrzeń, żeby nie gadał np. królik.jpg tylko królik
+            //images.Images.Keys[j].ToString().Replace(".jpg", "").Replace(".png", "")
+
+            for (int j = 0; j < images.Images.Count; j++)
+            {
+
+                this.picturesListView.Items.Add(images.Images.Keys[j].ToString().Replace(".jpg", "").Replace(".png", ""), j);
+            }
+        }
+        private void speakButton_Click(object sender, EventArgs e)
+        {
+
+            PromptBuilder.ClearContent();
+
+            for (int i = 0; i < picturesListView.Items.Count; i++)
+            {
+                PromptBuilder.AppendText(picturesListView.Items[i].Text);
+            }
+
+            // https://superuser.com/questions/590779/how-to-install-more-voices-to-windows-speech
+            // zainstalowałem wszystko pokazuje metoda zwraca, ale się nie chce odpalić zagadka dla Ciebie ;p
+            // var polishVoice = speechSynthesizer.GetInstalledVoices().First(x => x.VoiceInfo.Id == "TTS_MS_pl-PL_Paulina_11.0");
+            //  polishVoice.Enabled = true;
+            // speechSynthesizer.SelectVoice("Microsoft Server Speech Text to Speech Voice (pl-PL, Paulina");
+            speechSynthesizer.Speak(PromptBuilder);
+
+
+
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog = new OpenFileDialog();
+            OpenFileDialog.InitialDirectory = @"C:\";
+            OpenFileDialog.DefaultExt = "png";
+            OpenFileDialog.Filter = "png files (*.png)|*.jpg|All files (*.*)|*.*";
+            if (OpenFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                var path = OpenFileDialog.FileName;
+                GetImagesFromOpenDialog(path);
+            }
+        }
+
+        private void clearListView_Click(object sender, EventArgs e)
+        {
+            picturesListView.Clear();
+            images.Images.Clear();
+        }
+    }
+}
